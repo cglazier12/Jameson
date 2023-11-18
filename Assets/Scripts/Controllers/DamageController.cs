50a14e6895f94ae7c2d7be0679ebe8cc7756646e
Chris Glazier <chris.glazier12@gmail.com>
2023-11-18 00:00:59 -0500

    wip

--- Assets/Scripts/Controllers/DamageController.cs
+++ Assets/Scripts/Controllers/DamageController.cs
@@ -1,13 +1,32 @@
+using Entities;
 using UnityEngine;
 
 namespace Controllers
 {
     public class DamageController : MonoBehaviour
     {
-        public void ApplyDamage(Character character, float damageAmount)
+        public enum DamageType { Physical, Magical, Fire /* etc. can be removed or replaced with actual types */ }
+
+        // Delegate and event for damage application
+        public delegate void DamageApplied(Character character, float damageAmount, DamageType type);
+        public event DamageApplied OnDamageApplied;
+
+        public void ApplyDamage(Character character, float baseDamageAmount, DamageType type = DamageType.Physical)
         {
-            // The TakeDamage method would be called on the character
-            character.TakeDamage(damageAmount);
+            float finalDamage = CalculateFinalDamage(baseDamageAmount, type);
+            character.ReceiveDamage(finalDamage); // This method needs to be public or internally callable
+
+            // Trigger the event
+            OnDamageApplied?.Invoke(character, finalDamage, type);
         }
-    }    
+
+        private float CalculateFinalDamage(float baseDamage, DamageType type)
+        {
+            // Implementation to use 'type'
+            // For now, let's just return the base damage as a placeholder
+            return baseDamage;
+        }
+
+        // Other methods related to damage handling...
+    }
 }
--- Assets/Scripts/Entities/Character.cs
+++ Assets/Scripts/Entities/Character.cs
@@ -1,15 +1,43 @@
+using UnityEngine;
+using Controllers;
 
 namespace Entities
 {
-    public abstract class Character
+    public abstract class Character : MonoBehaviour
     {
-        protected float health;
+        [SerializeField] protected float health = 100f;
         protected MovementController movementController;
-        protected Animator animator;
+        protected Animator animator; // Ensure this is Unity's Animator
         protected DamageController damageController;
 
-        protected virtual void Move() { /* Common movement logic */ }
+        protected virtual void Awake()
+        {
+            // Initialize controllers
+            movementController = GetComponent<MovementController>();
+            animator = GetComponent<Animator>();
+            damageController = GetComponent<DamageController>();
+        }
+
+        protected virtual void Move(Vector2 direction)
+        {
+            movementController?.HandleMovement(direction);
+            // Additional move logic or animation triggers
+        }
+
         protected abstract void TakeDamage(float amount);
+
+        public virtual void ApplyDamage(float damage)
+        {
+            TakeDamage(damage);
+            // Check for health, death, etc.
+            if (health <= 0) HandleDeath();
+        }
+
+        protected virtual void HandleDeath()
+        {
+            // Death logic
+        }
+
         // Other common character methods...
     }
 }
